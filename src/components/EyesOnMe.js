054ce39484275c07a8c4f47970b8ab5b9b3dc4a8
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-04-24 21:34:29 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 class EyesOnMe extends React.Component {
+  
+  
   render() {
     return(
       <div>
@@ -13,3 +15,24 @@ class EyesOnMe extends React.Component {
 }
 
 export default EyesOnMe;
+
+
+
+
+import React from 'react'
+
+class KeyPad extends React.Component {
+  password = () => {
+    console.log('Entering password...');
+  }
+  
+  render() {
+    return(
+      <div>
+        <input keyUp={this.password} type="password" />
+      </div>
+    );
+  }
+}
+
+export default KeyPad;
