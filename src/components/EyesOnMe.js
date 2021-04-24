7b217161f595d50f5da4b20987d3fde7df5d7341
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-04-24 21:36:58 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -2,12 +2,20 @@ import React from 'react'
 
 class EyesOnMe extends React.Component {
   
+  focus = () => {
+    console.log('Good!');
+  }
+  
+  blur = () => {
+    console.log('Hey! Eyes on me!');
+  }
+  
   
   render() {
     return(
       <div>
         <button>
-          <button onFocus={this.focus}>Good! </button>
+          { onFocus={this.focus}>Good! 
         </button>
       </div>
     );
